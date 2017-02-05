a82f785e71c874e30545f82b88116e036d52cbdd
lucaa <lucaa@DESKTOP-O63FH7L>
2017-02-05 15:56:37 +0100

    niente

--- OrariDipendenti/Properties/AssemblyInfo.cs
+++ OrariDipendenti/Properties/AssemblyInfo.cs
@@ -11,14 +11,14 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Asilo Regina Margherita")]
 [assembly: AssemblyProduct("OrariDipendenti")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
 // ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
 // COM, impostare su true l'attributo ComVisible per tale tipo.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 //Per iniziare la compilazione delle applicazioni localizzabili, impostare
 //<UICulture>CultureYouAreCodingWith</UICulture> nel file .csproj
@@ -48,6 +48,6 @@ using System.Windows;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9.0")]
-[assembly: AssemblyFileVersion("1.0.9.0")]
+[assembly: AssemblyVersion("1.0.9.1")]
+[assembly: AssemblyFileVersion("1.0.9.1")]
 [assembly: NeutralResourcesLanguage("en")]
