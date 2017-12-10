150e4e3e8c1a6a1281879c6ac35ff2cd22dba5b6
lucaa <lucaa@DESKTOP-O63FH7L>
2017-12-10 11:10:06 +0100

    niente ...

--- OrariDipendenti/Properties/AssemblyInfo.cs
+++ OrariDipendenti/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.11.4")]
-[assembly: AssemblyFileVersion("1.0.11.4")]
+[assembly: AssemblyVersion("1.0.11.5")]
+[assembly: AssemblyFileVersion("1.0.11.5")]
 [assembly: NeutralResourcesLanguage("en")]
